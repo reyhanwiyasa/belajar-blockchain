de5513b714082b5e7685bc7050fe07973b79f4a7
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-06-26 13:28:59 +0700

    [GREEN] implement blockchain class

--- blockchain.js
+++ blockchain.js
@@ -1,2 +1,16 @@
-class Blockchain {}
+const Block = require("./block");
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data,
+    });
+    this.chain.push(newBlock);
+  }
+}
 module.exports = Blockchain;
